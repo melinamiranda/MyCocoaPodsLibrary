cd4df812e8fc5ac13d272189e6728547d93e6426
Miranda Melina <mmiranda@yopapp.me>
2020-11-04 12:22:53 -0300

    finished the pod

---
+++ MyCocoaPodsLibrary/MyCocoaPodsLibrary/MyCocoaPodsLibrary.h
@@ -0,0 +1,18 @@
+//
+//  MyCocoaPodsLibrary.h
+//  MyCocoaPodsLibrary
+//
+//  Created by Melina Miranda on 04/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyCocoaPodsLibrary.
+FOUNDATION_EXPORT double MyCocoaPodsLibraryVersionNumber;
+
+//! Project version string for MyCocoaPodsLibrary.
+FOUNDATION_EXPORT const unsigned char MyCocoaPodsLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyCocoaPodsLibrary/PublicHeader.h>
+
+
---
+++ MyCocoaPodsLibrary/MyCocoaPodsLibrary/SomeFunctionality.swift
@@ -0,0 +1,17 @@
+//
+//  SomeFunctionality.swift
+//  MyCocoaPodsLibrary
+//
+//  Created by Melina Miranda on 04/11/2020.
+//
+
+import Foundation
+
+public class SomeFunctionality {
+    private init() {}
+    
+    func doSomething() -> String{
+        return "Returning the string"
+    }
+}
+
